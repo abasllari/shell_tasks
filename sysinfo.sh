b5086b40c834c8d8912a954ed6ce11fc28f192b7
Daniel Abasllari <abasllaridaniel@gmail.com>
2021-08-12 14:52:12 +0200

    put it on log file

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+set -euo pipefail
 
 echo "your system info are as beleow:"
-
+echo "your memory is $(free -h | grep Mem | awk '{ print $2 }' ) "
 echo "you have  $(cat /proc/cpuinfo | grep processor | wc -l) processors "  #how many processors you have
-var=$( df -h | grep root | awk '{ print $5}') 
-echo "you file systeim is used $var" 
+var=$( df -h | grep "/$"  | awk '{ print $5}') 
+echo "you file system is used $var" 
 
 if  [ "$var">=10 ] 
 then
-	echo "you will have an email"
+	echo "you will have an email"  #next task send it by email
+	echo  "you file system is used $var" >> $HOME/sysinfo.log
 
 else  	echo "not having email"
 fi
